880f013f85a7104a9a42b61348f69a08acab411b
xdorijanx <dorijan11@gmail.com>
2019-09-07 18:47:08 +0200

    learned to use -- --coverage

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -1,10 +1,4 @@
-function filterByTerm(inputArr, searchTerm) {
-    const regex = new RegExp(searchTerm, "i");
-    return inputArr.filter((arrayElement) => {
-        
-        return arrayElement.url.match(regex);
-    });
-}
+const filterByTerm = require("../src/filterByTerm");
 
 describe("Filter Function", () => {
     //test stuff
@@ -35,6 +29,23 @@ describe("Filter Function", () => {
         ]
 
         expect (filterByTerm(input, "https://www.url1.dev")).toEqual(output)
+    })
+
+    test("it should throw an error when inputArr is empty ()", () => {
+        const input={}
+
+        expect (filterByTerm(input, "[]")).toEqual("inputArr cannot be empty")
+    })
+
+
+       test("it should throw an error when searchTerm is empty ()", () => {
+        const input = [
+            { id: 1, url: "https://www.url1.dev"},
+            { id: 2, url: "https://www.url2.dev"},
+            { id: 3, url: "https://www.link3.dev"},
+        ]
+
+        expect (filterByTerm(input, )).toEqual("searchTerm cannot be empty")
     })
 
 })
